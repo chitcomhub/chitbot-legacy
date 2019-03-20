c968f49881144d6a85b44120917b294bfa8e1924
ai-rage <azamat.murdalov@mail.ru>
2019-03-20 23:43:21 +0300

    add sqlite3 and connect with bot

--- entry.py
+++ entry.py
@@ -1,17 +1,45 @@
 import requests
-import configurations
+import sqlite3
+import configurations # configurations.py содержит токен
 
+# соединение с SQLite3
+def create_connection():
+	conn = sqlite3.connect("champ.db")
+	cursor = conn.cursor()
+
+	sql = "SELECT rowid, * FROM tasks ORDER BY title"
+	tasks = [row for row in cursor.execute(sql)]
+
+	# закрываем базу
+	conn.close()
+	
+	return(tasks)
+	
 def point(event, context):
 	print(event)
-	if event["message"]["text"][0] == "/":
+
+	message_text = event["message"]["text"]
+	chat_id = event["message"]["chat"]["id"]
+	chiter = event["message"]["from"]["username"]
+	tasks = create_connection()
+
+	if message_text[0] == "/":
 		words = event["message"]["text"].split()
 		command = words[0][1:]
 		if command == "start":
-			champ_text = "Начнем турнир по программированию."
-			send_message(event["message"]["chat"]["id"], champ_text)
+			champ_text = "Начнем турнир по программированию"
+			send_message(chat_id, champ_text)
+		elif command == "tasks":
+			for i in tasks:
+				task = "%s: \n %s" % (i[1], i[2])
+				send_message(chat_id, task)
 		elif command == "help":
-			help_text = "Вы можете решать задачи на любом языке программирования."
-			send_message(event["message"]["chat"]["id"], help_text)
+			help_text = "Вы можете решать задачи на любом языке программирования"
+			send_message(chat_id, help_text)
+	for i in tasks:
+		if message_text in i:
+			answer = "Участник @%s решил Задачу №%s\nОтвет: %s" % (chiter, i[0], i[3])
+			send_message(chat_id, answer)
 
 def send_message(chat_id, text):
 	url = "https://api.telegram.org/bot{token}/{method}".format(
